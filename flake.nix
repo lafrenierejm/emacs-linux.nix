f6dd6fb3fd609c22705842b9837a035164181366
Ben Ide <ben@bencide.com>
2023-01-05 23:21:06 -0500

    removed old emacs build.
    it's worth looking back at this if I ever need to use emacs on a mac.

--- flake.nix
+++ flake.nix
@@ -1,25 +1,14 @@
 {
   description = "Emacs with vterm";
 
-  # TODO: add nix-community/emacs-overlay, otherwise this won't work.
-
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     emacs-overlay = {
       url = github:nix-community/emacs-overlay;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # emacs-vterm-src = {
-    #   url = github:akermu/emacs-libvterm;
-    #   flake = false;
-    # };
-    # emacs-src = {
-    #   url = git+https://git.savannah.gnu.org/git/emacs.git;
-    #   flake = false;
-    # };
   };
 
-  # outputs = { self, nixpkgs, emacs-overlay, emacs-vterm-src, emacs-src, ... }:
   outputs = { self, nixpkgs, emacs-overlay, ... }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
@@ -32,85 +21,12 @@
 
       packages = forAllSystems (system: {
         emacs = nixpkgsFor.${system}.emacs;
-        # emacsOld = nixpkgsFor.${system}.emacsOld;
-        # emacs-vterm = nixpkgsFor.${system}.emacs-vterm;
         emacs-i3-integration = nixpkgsFor.${system}.emacs-i3-integration;
         emacs-sway-integration = nixpkgsFor.${system}.emacs-sway-integration;
         emacs-xsway-integration = nixpkgsFor.${system}.emacs-xsway-integration;
       });
 
       overlay = final: prev: {
-
-        # emacs-vterm = prev.stdenv.mkDerivation rec {
-        #   pname = "emacs-vterm";
-        #   version = "master";
-
-        #   src = emacs-vterm-src;
-
-        #   nativeBuildInputs = [
-        #     prev.cmake
-        #     prev.libtool
-        #     prev.glib.dev
-        #   ];
-
-        #   buildInputs = [
-        #     prev.glib.out
-        #     prev.libvterm-neovim
-        #     prev.ncurses
-        #   ];
-
-        #   cmakeFlags = [
-        #     "-DUSE_SYSTEM_LIBVTERM=yes"
-        #   ];
-
-        #   preConfigure = ''
-        #     echo "include_directories(\"${prev.glib.out}/lib/glib-2.0/include\")" >> CMakeLists.txt
-        #     echo "include_directories(\"${prev.glib.dev}/include/glib-2.0\")" >> CMakeLists.txt
-        #     echo "include_directories(\"${prev.ncurses.dev}/include\")" >> CMakeLists.txt
-        #     echo "include_directories(\"${prev.libvterm-neovim}/include\")" >> CMakeLists.txt
-        #   '';
-
-        #   installPhase = ''
-        #     mkdir -p $out
-        #     cp ../vterm-module.so $out
-        #     cp ../vterm.el $out
-        #   '';
-        # };
-
-        # emacsOld = (prev.emacs.override {
-        #   srcRepo = true;
-        #   nativeComp = true;
-        #   withSQLite3 = true;
-        #   withGTK3 = true;
-        #   # withPgtk = true;
-        # }).overrideAttrs (
-        #   old: rec {
-        #     version = "30.0.50";
-        #     src = emacs-src;
-        #     patches = [ ];
-        #     postPatch = old.postPatch + ''
-        #       substituteInPlace lisp/loadup.el \
-        #       --replace '(emacs-repository-get-branch)' '"master"'
-        #     '';
-
-        #     configureFlags = [
-        #                        "--disable-build-details"
-        #                        # "--with-pgtk"
-        #                        "--with-native-compilation"
-        #                        "--with-xinput2"
-        #                      ];
-
-        #     # I don't know which one of these is correct
-        #     # NIX_CFLAGS_COMPILE = [ (prev.NIX_CFLAGS_COMPILE or "") ]
-        #     #                      ++ [ "-O3" "-march=native" "-fPIC" "-fomit-frame-pointer" ];
-        #     CFLAGS = "-O3 -march=native -fPIC -fomit-frame-pointer";
-
-        #     postInstall = old.postInstall + ''
-        #       cp ${final.emacs-vterm}/vterm.el $out/share/emacs/site-lisp/vterm.el
-        #       cp ${final.emacs-vterm}/vterm-module.so $out/share/emacs/site-lisp/vterm-module.so
-        #     '';
-        #   }
-        # );
 
         emacs = ((prev.emacsPackagesFor (prev.emacsGit.overrideAttrs (
           old: rec {
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 ## Emacs with vterm on NixOS
 
-This flake provides an overlay of emacs with vterm. It is used to be a modification of [this](https://github.com/cmacrae/emacs/blob/master/flake.nix), but that's now commented out and a much simpler version is left in its place. This is really just for personal use, but feel free to use anything you think would be useful.
+This flake provides an overlay of emacs with vterm. It is used to be a modification of [this](https://github.com/cmacrae/emacs/blob/master/flake.nix), but that's now removed and a much simpler version is left in its place. This is really just for personal use, but feel free to use anything you think would be useful.
 
 ## i3 and Sway integration
 
 A few scripts are included as packages for integration with i3 and Sway as well. They require some emacs functions to be defined, some global i3/Sway keybindings, and the use of an emacs daemon.
 
-- TODO: add the details for this
+- TODO: add details
 
 ## Note to self
 
-- I've explicitly overridden `configureFlags`. The `--disable-build-details` flag just makes the build slightly more reproducible.
+- I've explicitly overridden `configureFlags`. The `--disable-build-details` flag just makes the build slightly more reproducible. I could probably stick with defaults instead.
 - I'm not sure if I need so many CFLAGS... `CFLAGS='-O2 -march=native'` is probably fine.
-- For loading vterm with `straight` and `straight-use-package`, I'm not sure if I should `(use-package vterm :straight nil :ensure nil)` or if `(use-package vterm)` is ok. Probably the latter is fine, but it does end up pulling some unnecessary stuff. Since the module is already available, it doesn't try to compile it again. The benefit is that the latter will still work on another system that doesn't include vterm.
+- Both of the above are to make it more like what I do without nix:
 
 Without nix, I usually configure like this for a wayland build:
 ```
@@ -32,4 +32,4 @@ or like this for an X build (sometimes with the addition of `--with-x-toolkit=lu
 make -j$(nproc)
 ```
 
-details about the CFLAGS can be found [here](https://wiki.gentoo.org/wiki/GCC_optimization#Optimizing).
+Details about the CFLAGS can be found [here](https://wiki.gentoo.org/wiki/GCC_optimization#Optimizing).
